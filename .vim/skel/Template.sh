ede8fa6793d3e06a18acd984b81c57f8efbc6d59
aptx4869 <ling548@gmail.com>
2012-07-03 20:04:12 +0800

    back up before big change

---
+++ .vim/skel/Template.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#-------------------------------------------------
+#     Author: <T_AUTHOR> <<T_AUTHOR_EMAIL>>
+#     <T_AUTHOR_WEBSITE>
+#  
+#     File: <T_FILENAME>
+#     Create Date: <T_CREATE_DATE>
+#-------------------------------------------------
+
